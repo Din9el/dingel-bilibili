ad148bc700643e229f1ef9f490f2ebdc716b88d8
Dingel <1837592441@qq.com>
2022-09-05 21:32:15 +0800

    second commit

--- dingel-bilibili-api/src/main/java/com/dingel/bilibili/api/RestfulDemoApi.java
+++ dingel-bilibili-api/src/main/java/com/dingel/bilibili/api/RestfulDemoApi.java
@@ -9,39 +9,52 @@ import java.util.Map;
 @RestController
 public class RestfulDemoApi {
 
-    private final Map<Integer,Map<String,Object>> dataMap;
-
+    private final Map<Integer, Map<String, Object>> dataMap;
 
     public RestfulDemoApi(){
         dataMap = new HashMap<>();
-        for(int i=1;1<4;i++){
-            Map<String,Object> data = new HashMap<>();
-            data.put("id",i);
-            data.put("name","name"+i);
-            dataMap.put(i,data);
+        for(int i = 1; i<3; i++){
+            Map<String, Object> data = new HashMap<>();
+            data.put("id", i);
+            data.put("name", "name"+i);
+            dataMap.put(i, data);
         }
     }
 
-    @GetMapping("/obj/{id}")
-    public Map<String,Object> getData(@PathVariable Integer id){
+    @GetMapping("/objects/{id}")
+    public Map<String, Object> getData(@PathVariable Integer id){
         return dataMap.get(id);
     }
 
-    @DeleteMapping("/obj/{id}")
-    public String DeleteData(@PathVariable Integer id){
+    @DeleteMapping("/objects/{id}")
+    public String deleteData(@PathVariable Integer id){
         dataMap.remove(id);
         return "delete success";
     }
 
-    @PostMapping("/obj")
-    public String postData(@RequestBody Map<String,Object> data){
-        Integer [] idArray = dataMap.keySet().toArray(new Integer[0]);
+    @PostMapping("/objects")
+    public String postData(@RequestBody Map<String, Object> data){
+        Integer[] idArray = dataMap.keySet().toArray(new Integer[0]);
         Arrays.sort(idArray);
-        int nextId = idArray[idArray.length-1]+1;
-        dataMap.put(nextId,data);
+        int nextId = idArray[idArray.length - 1] + 1;
+        dataMap.put(nextId, data);
         return "post success";
     }
 
+    @PutMapping("/objects")
+    public String putData(@RequestBody Map<String, Object> data){
+        Integer id = Integer.valueOf(String.valueOf(data.get("id")));
+        Map<String, Object> containedData = dataMap.get(id);
+        if(containedData == null){
+            Integer[] idArray = dataMap.keySet().toArray(new Integer[0]);
+            Arrays.sort(idArray);
+            int nextId = idArray[idArray.length - 1] + 1;
+            dataMap.put(nextId, data);
+        }else{
+            dataMap.put(id, data);
+        }
+        return "put success";
+    }
 
 
 
